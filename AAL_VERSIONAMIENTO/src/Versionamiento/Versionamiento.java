6ab1cd5b9b76b3b83d3e47317fca8182979b78bb
WilsonCastro <orochi_cris@yahoo.com>
2012-10-25 17:30:28 +0200

    generacion del proyecto

---
+++ AAL_VERSIONAMIENTO/src/Versionamiento/Versionamiento.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Versionamiento;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Wilson
+ */
+public class Versionamiento {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "OK EN EL VERSIONAMINETO");
+    }
+}
---
+++ AAL_VERSIONAMIENTO/src/aal_versionamiento/AAL_VERSIONAMIENTO.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aal_versionamiento;
+
+/**
+ *
+ * @author Wilson
+ */
+public class AAL_VERSIONAMIENTO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
