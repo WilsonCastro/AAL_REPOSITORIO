92db11df60d4a5bd6fb49af873a20bd43e077f34
WilsonCastro <orochi_cris@yahoo.com>
2012-10-25 17:47:53 +0200

    Creacion de proyecto

---
+++ src/githubtest/GithubTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubtest;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Wilson
+ */
+public class GithubTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "Prueba de GitHUB");
+    }
+}
